5f9de3033dee5706c1b9bd379edde0c1f50e024f
SWATI PRIYA <115929127+missRobo7280@users.noreply.github.com>
2025-04-07 18:54:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import serial
 import time
 
-# Connect to COM8 at 9600 baud rate
+
 try:
     ser = serial.Serial('COM8', 9600, timeout=1)
     time.sleep(2)  # Wait for Arduino to reset
 
-    ser.write(b"C")  # Send the character 'C'
-    print("✅ Sent 'C' to COM8")
+    ser.write(b"B")
+    print("✅ Sent 'B' to COM8")
 
     ser.close()
     print("🔌 Serial connection closed.")
